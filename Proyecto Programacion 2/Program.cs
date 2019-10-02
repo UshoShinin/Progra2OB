eaf9da3c499527f13c46712a451655dbf96f935f
Usho <ushoivanjose@gmail.com>
2019-10-02 18:44:39 -0300

    Subido maquinola

---
+++ Proyecto Programacion 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Proyecto_Programacion_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mama, estoy en la consola");
+            Console.ReadKey();
+        }
+    }
+}
