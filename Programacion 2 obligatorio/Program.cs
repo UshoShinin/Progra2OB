105952dda06f40c10322ef9d913496e84bc962aa
Usho <ushoivanjose@gmail.com>
2019-10-14 09:13:43 -0300

    PediNumero

--- Programacion 2 obligatorio/Program.cs
+++ Programacion 2 obligatorio/Program.cs
@@ -11,8 +11,38 @@ namespace Programa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola");
-            Console.ReadLine();
+            int opc = PedirNumero("Ingrese una opcion entre 1 y 3", "El valor debe ser numérico y estar entre 1 y 3", 1, 3);
+            switch (opc)
+            {
+                case 1:
+                    Console.WriteLine("1");
+                    break;
+                case 2:
+                    Console.WriteLine("2");
+                    break;
+                case 3:
+                    Console.WriteLine("3");
+                    break;
+            }
         }
+        public static int PedirNumero(string msg, string msgError, int min, int max){
+            int opc;
+            bool exito = false;
+            do
+            {
+                Console.WriteLine(msg);
+                string sOpc = Console.ReadLine();
+                if (int.TryParse(sOpc, out opc) && (opc >= min && opc <= max)){
+                    exito = true;
+                }
+                else
+                {
+                    Console.WriteLine(msgError);
+                }
+
+            } while (!exito);
+            return opc;
+        }
+
     }
 }
